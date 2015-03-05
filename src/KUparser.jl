22af18954e2f872ee02d8979261486fb5cc386c0
Deniz Yuret <denizyuret@gmail.com>
2015-03-05 17:07:24 +0200

    *** empty log message ***

--- src/KUparser.jl
+++ src/KUparser.jl
@@ -2,10 +2,13 @@ module KUparser
 
 using InplaceOps
 using KUnet
+# ?? @everywhere using KUnet
 
 typealias Fmat Matrix{Int8}
 type Sentence form; lemma; cpostag; postag; head; deprel; wvec; Sentence()=new(); end
 typealias Corpus Vector{Sentence}
+wdim(s)=size(s.wvec,1)
+wcnt(s)=size(s.wvec,2)
 
 include("archybrid.jl")
 include("features.jl")
