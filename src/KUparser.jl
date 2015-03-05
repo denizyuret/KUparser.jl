7fadab72c06adec9680d6739051c686a6fbb8281
Deniz Yuret <denizyuret@gmail.com>
2015-03-05 15:29:30 +0200

    testing with d1.jl.

--- src/KUparser.jl
+++ src/KUparser.jl
@@ -1,3 +1,17 @@
 module KUparser
+
+using InplaceOps
+using KUnet
+
+typealias Fmat Matrix{Int8}
+type Sentence form; lemma; cpostag; postag; head; deprel; wvec; Sentence()=new(); end
+typealias Corpus Vector{Sentence}
+
 include("archybrid.jl")
+include("features.jl")
+include("gparser.jl")
+
+# Testing:
+include("d1.jl")
+
 end
