2425f60909c38f36ed5c8d2541a19df224c295e5
Deniz Yuret <denizyuret@gmail.com>
2015-03-07 17:03:14 +0200

    *** empty log message ***

--- src/KUparser.jl
+++ src/KUparser.jl
@@ -2,15 +2,13 @@ module KUparser
 
 using InplaceOps
 using KUnet
+using Compat
 
 typealias Features Matrix{Int8}
 type Sentence form; lemma; cpostag; postag; head; deprel; wvec; Sentence()=new(); end
 typealias Corpus AbstractVector{Sentence}
 wdim(s)=size(s.wvec,1)
 wcnt(s)=size(s.wvec,2)
-
-isdefined(:UInt16) || typealias UInt16 Uint16
-isdefined(:UInt8) || typealias UInt16 Uint8
 
 include("archybrid.jl")
 include("features.jl")
