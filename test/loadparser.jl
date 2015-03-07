bf732ffcb10dea37069ed7c3986bf22079e412f8
Deniz Yuret <denizyuret@gmail.com>
2015-03-07 17:04:56 +0200

    *** empty log message ***

--- test/loadparser.jl
+++ test/loadparser.jl
@@ -2,7 +2,7 @@ using KUnet
 using MAT
 include("../src/KUparser.jl")
 include("../src/flist.jl")
-isdefined(:d1) || (f=matopen("d1.mat");d1=read(f,"d1");close(f))
+isdefined(:d1mat) || (f=matopen("d1.mat");d1mat=read(f,"d1");close(f))
 
 function readcorpus(m)
     d = KUparser.Sentence[]
@@ -15,9 +15,9 @@ function readcorpus(m)
     return d
 end
 
-dev=readcorpus(d1["dev"])
-trn=readcorpus(d1["trn"])
-tst=readcorpus(d1["tst"])
+dev=readcorpus(d1mat["dev"])
+trn=readcorpus(d1mat["trn"])
+tst=readcorpus(d1mat["tst"])
 s=dev[1]
 p=KUparser.ArcHybrid(size(s.wvec,2))
 v=KUparser.valid(p)
