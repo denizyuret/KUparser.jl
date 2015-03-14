193969e4fd1a05d42f6f3b2f6b2056b0019b22d4
Deniz Yuret <denizyuret@gmail.com>
2015-03-14 11:45:07 +0200

    sortperm! with AbstractVector.

--- src/KUparser.jl
+++ src/KUparser.jl
@@ -23,4 +23,14 @@ include("features.jl")
 include("gparser.jl")
 include("bparser.jl")
 
+# this has been fixed in Julia 0.4:
+import Base: sortperm!, Algorithm, Ordering, DEFAULT_UNSTABLE, Forward, Perm, ord
+function sortperm!{I<:Integer}(x::AbstractVector{I}, v::AbstractVector; alg::Algorithm=DEFAULT_UNSTABLE,
+                               lt::Function=isless, by::Function=identity, rev::Bool=false, order::Ordering=Forward,
+                               initialized::Bool=false)
+    length(x) != length(v) && throw(ArgumentError("Index vector must be the same length as the source vector."))
+    !initialized && @inbounds for i = 1:length(v); x[i] = i; end
+    sort!(x, alg, Perm(ord(lt,by,rev,order),v))
+end
+
 end
