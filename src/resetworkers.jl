50d01d2845bb9e4f59f9d2beb8358cc9b12b33ce
Deniz Yuret <denizyuret@gmail.com>
2015-03-22 22:15:48 +0200

    *** empty log message ***

---
+++ src/resetworkers.jl
@@ -0,0 +1,11 @@
+# This restarts workers, need this until they fix memory leaks.
+# Unfortunately it doesn't work from within the module.
+function resetworkers(ncpu)
+    nprocs() > 1 && rmprocs(workers())
+    addprocs(ncpu)
+    require("CUDArt")
+    @everywhere CUDArt.device((myid()-1) % CUDArt.devcount())
+    require("CUBLAS")
+    require("KUnet")
+    require("KUparser")
+end # resetworkers
