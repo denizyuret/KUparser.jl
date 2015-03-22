6cd6f9dca67efb17c09cf25179c360417815e82e
Deniz Yuret <denizyuret@gmail.com>
2015-03-22 21:47:25 +0200

    Tested with deprel.

--- src/oparser.jl
+++ src/oparser.jl
@@ -1,51 +1,51 @@
-# The oracle parsers executes the moves that will lead to the best
-# parse:
+# The oracle parser executes the moves that will lead to the best parse:
 
-function oparse(s::Sentence, f::Features)
-    (p, x, y, c) = initoparse(s, f)
-    nx = 0
-    while (v = valid(p); any(v))
+function oparse(s::Sentence, f::Features, ndeps::Integer)
+    (p, x, y, c, v) = initoparse(s, f, ndeps)
+    nx = 0; cost = 0
+    while any(validmoves(p,v))
         nx += 1
         features(p, s, f, sub(x,:,nx:nx))
-        cost(p, s.head, c)
-        bestmove = indmin(c)
+        movecosts(p, s.head, s.deprel, c)
+        (bestcost,bestmove) = findmin(c)
+        cost += bestcost
         y[bestmove, nx] = one(eltype(y))
         move!(p, bestmove)
     end
-    (p.head, x, y)
+    @assert nx == size(x,2)
+    @assert cost == sum((p.head .!= s.head) | (p.deprel .!= s.deprel))
+    (p, x, y)
 end
 
-function oparse(c::Corpus, f::Features)
-    p = map(s->oparse(s,f), c)
-    h = map(z->z[1], p)
-    x = hcat(map(z->z[2], p)...)
-    y = hcat(map(z->z[3], p)...)
-    (h, x, y)
+function oparse(c::Corpus, f::Features, ndeps::Integer)
+    pxy = map(s->oparse(s,f,ndeps), c)
+    p = map(z->z[1], pxy)
+    x = hcat(map(z->z[2], pxy)...)
+    y = hcat(map(z->z[3], pxy)...)
+    (p, x, y)
 end
 
-function oparse(c::Corpus, f::Features, ncpu::Integer)
-    assert(nworkers() >= ncpu)
+function oparse(c::Corpus, f::Features, ndeps::Integer, ncpu::Integer)
+    Main.resetworkers(ncpu)
     d = distproc(c, workers()[1:ncpu])
-    @everywhere gc()
-    p = pmap(procs(d)) do x
-        oparse(localpart(d), f)
+    pxy = pmap(procs(d)) do x
+        oparse(localpart(d), f, ndeps)
     end
-    # d=nothing; @everywhere gc()
-    # h = vcat(map(z->z[1], p)...)
-    # x = hcat(map(z->z[2], p)...)
-    # y = hcat(map(z->z[3], p)...)
-    # p=nothing; @everywhere gc()
-    # (h, x, y)
+    p = vcat(map(z->z[1], pxy)...)
+    x = hcat(map(z->z[2], pxy)...)
+    y = hcat(map(z->z[3], pxy)...)
+    (p, x, y)
 end
 
-function initoparse(s::Sentence, f::Features)
+function initoparse(s::Sentence, f::Features, ndeps::Integer)
     (ndims, nword) = size(s.wvec)
     xtype = eltype(s.wvec)
     xrows = flen(ndims, f)
     xcols = 2 * (nword - 1)
-    p = ArcHybrid(nword)
+    p = ArcHybrid(nword, ndeps)
     x = Array(xtype, xrows, xcols)
     y = zeros(xtype, p.nmove, xcols)
     c = Array(Pval,p.nmove)
-    (p, x, y, c)
+    v = Array(Bool,p.nmove)
+    (p, x, y, c, v)
 end
