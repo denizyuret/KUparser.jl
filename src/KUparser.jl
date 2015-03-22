ed7c79e3a73eb9c5e9daa21c90cea5391987824a
Deniz Yuret <denizyuret@gmail.com>
2015-03-22 22:18:17 +0200

    Cleanup.

--- src/KUparser.jl
+++ src/KUparser.jl
@@ -1,42 +1,22 @@
 module KUparser
 
-using InplaceOps
-using KUnet
 using Compat
-if VERSION >= v"0.4.0-dev+3184"
-    using DistributedArrays
-end
+using KUnet
 
-typealias Features Matrix{Int8}
-type Sentence form; lemma; cpostag; postag; head; deprel; wvec; Sentence()=new(); end
+type Sentence form; postag; head; deprel; wvec; Sentence()=new(); end
 typealias Corpus AbstractVector{Sentence}
 wdim(s)=size(s.wvec,1)
 wcnt(s)=size(s.wvec,2)
 
-# Type representing feature values
-typealias Fval Float32
-typealias Fvec Vector{Fval}
-typealias Fmat Matrix{Fval}
+include("util.jl");	  export @date
+include("archybrid.jl");  # export ArcHybrid
+include("flist.jl");      export Features, Flist 	# these are feature templates
+include("features.jl");   export features, flen, fidx   # these extract features
+include("gparser.jl");    export gparse
+include("bparser.jl");    export bparse
+include("oparser.jl");    export oparse
+include("rparser.jl");    export rparse
 
-include("archybrid.jl")
-include("features.jl")
-include("gparser.jl")
-include("bparser.jl")
-include("oparser.jl")
-include("rparser.jl")
-include("flist.jl")
+end # module
 
-# this has been fixed in Julia 0.4:
-if VERSION >= v"0.4.0-dev+3184"
-    import Base.sortperm!
-end
-using Base: Algorithm, Ordering, DEFAULT_UNSTABLE, Forward, Perm, ord
-function sortperm!{I<:Integer}(x::AbstractVector{I}, v::AbstractVector; alg::Algorithm=DEFAULT_UNSTABLE,
-                               lt::Function=isless, by::Function=identity, rev::Bool=false, order::Ordering=Forward,
-                               initialized::Bool=false)
-    length(x) != length(v) && throw(ArgumentError("Index vector must be the same length as the source vector."))
-    !initialized && @inbounds for i = 1:length(v); x[i] = i; end
-    sort!(x, alg, Perm(ord(lt,by,rev,order),v))
-end
-
-end
+include("resetworkers.jl");
