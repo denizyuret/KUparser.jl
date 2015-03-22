635f50c982e82c66224ec90873cdec63420c3331
Deniz Yuret <denizyuret@gmail.com>
2015-03-22 23:18:20 +0200

    *** empty log message ***

--- src/KUparser.jl
+++ src/KUparser.jl
@@ -3,12 +3,13 @@ module KUparser
 using Compat
 using KUnet
 
+abstract Parser
 type Sentence form; postag; head; deprel; wvec; Sentence()=new(); end
 typealias Corpus AbstractVector{Sentence}
 wdim(s)=size(s.wvec,1)
 wcnt(s)=size(s.wvec,2)
 
-include("util.jl");	  export @date
+include("util.jl");	  export @date, evalparse
 include("archybrid.jl");  # export ArcHybrid
 include("flist.jl");      export Features, Flist 	# these are feature templates
 include("features.jl");   export features, flen, fidx   # these extract features
