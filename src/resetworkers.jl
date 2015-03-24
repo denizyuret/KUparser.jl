e23df6cbca43568c612d633134e41901346ba05f
Deniz Yuret <denizyuret@gmail.com>
2015-03-24 15:16:36 +0200

    Add sleep after rmprocs, otherwise get stream error.

--- src/resetworkers.jl
+++ src/resetworkers.jl
@@ -1,7 +1,7 @@
 # This restarts workers, need this until they fix memory leaks.
 # Unfortunately it doesn't work from within the module.
 function resetworkers(ncpu)
-    nprocs() > 1 && rmprocs(workers())
+    nprocs() > 1 && (rmprocs(workers());sleep(1))
     addprocs(ncpu)
     require("CUDArt")
     @everywhere CUDArt.device((myid()-1) % CUDArt.devcount())
