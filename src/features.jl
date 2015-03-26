15483a2150becf1684ecd72829b3cc9e7bcdaaa6
Deniz Yuret <denizyuret@gmail.com>
2015-03-26 17:53:36 +0200

    New features based on strings

--- src/features.jl
+++ src/features.jl
@@ -1,295 +1,105 @@
-# f::Features is a nx3 matrix whose rows determine which features to extract
-# Each row of f consists of the following three values:
-# 1. anchor word: 0:n0, 1:n1, 2:n2, ..., -1:s0, -2:s1, -3:s2, ...
-# 2. target word: 0:self, 1:rightmost child, 2:second rightmost, -1:leftmost, -2:second leftmost ...
-# 3. feature: One of the features listed below.
+# featurevec() returns a feature vector given a parser, a sentence,
+# and a FeatureList.  An optional last argument can provide the
+# preallocated output vector which should have length flen().
 #
-# 0:wvec (word+context if token, dim depends on encoding)
-# +-1: exists/doesn't (one bit)
-# +-2: right/left child count (4 bits, == encoding: 0,1,2,3+)
-# +3: distance to right (4 bits, == encoding: 1,2,3,4+, root is 4+)
-# -3: average of in-between tokens to the right (dim:wvec)
-# +-4: word/context half of vector (dim:wvec/2, only valid for token encoding, assumes first half=word, second half=context)
-# +-5: right/left child count (4 bits, >= encoding: >=1, >=2, >=3, >=4)
-# +-6: right/left child count (4 bits, <= encoding: <=0, <=1, <=2, <=3)
-# +7: distance to right, >= encoding (8 bits, >= encoding: >=2, >=3, >=4, >=6, >=8, >=12, >=16, >=20)
-# -7: distance to right, <= encoding (8 bits, <= encoding: <=1, <=2, <=3, <=4, <=6, <=8, <=12, <=16)
-# +-8: average of in-between word/context vectors to the right (dim:wvec/2)
-# +-9: head exists/doesn't (one bit)
+# A FeatureList is a ASCIIString array specifying a set of features.
+# Each string specifies a particular feature and has the form:
+#   [sn]\d?([hlr]\d?)*[wpdLabAB]
+# 
+# The meaning of each letter is below.  In the following i is a single
+# digit integer which is optional if the default value is used:
+#
+# si: i'th stack word, default i=0 means top
+# ni: i'th buffer word, default i=0 means first
+# hi: i'th degree head, default i=1 means direct head
+# li: i'th leftmost child, default i=1 means the leftmost child 
+# ri: i'th rightmost child, default i=1 means the rightmost child
+# w: word vector
+# p: context vector
+# d: distance to the right.  e.g. s1d is s0s1 distance, s0d is s0n0
+#    distance.  encoding: 1,2,3,4,5-10,10+ (from ZN11)
+# L: dependency label (0 is ROOT or NONE)
+# a: number of left children.  encoding: 0,1,...,8,9+
+# b: number of right children.  encoding: 0,1,...,8,9+
+# A: set of left dependency labels
+# B: set of right dependency labels
 
-const flength = Int[1, -1, 8, 4, 4, -1, -2, 4, 1, -2, 1, 4, 4, -1, 4, 4, 8, -1, 1]
-const foffset = 10
+typealias FeatureList Vector{ASCIIString}
 
-function flen(ndim::Int, f::Features)
-    nfeat = size(f, 1)
-    ndim2 = ndim>>1
-    len = 0;
-    for i=1:nfeat
-        l = flength[f[i,3] + foffset]
-        len += (l > 0 ? l : -l*ndim2)
-    end
-    return len
-end
-
-# fidx[ifeat]: end of feature f[ifeat,:] in x
-
-function fidx(ndim::Int, f::Features)
-    nfeat = size(f, 1)
-    ndim2 = ndim>>1
-    idx = Array(Int, nfeat)
-    for i=1:nfeat
-        l = flength[f[i,3] + foffset]
-        idx[i] = (i > 1 ? idx[i-1] : 0) + (l > 0 ? l : -l*ndim2)
-    end
-    return idx
-end
-
-function features(p::Parser, s::Sentence, f::Features, x::AbstractArray=Array(eltype(s.wvec),flen(size(s.wvec,1),f),1))
-
-    # Given a parser state p, a sentence s, and a feature matrix f, fills and
-    # returns a feature vector x, which should be preallocated to have
-    # flen(size(s.wvec,1), f) dimensions.  x will be allocated if not given.
-
-    ndim = size(s.wvec,1)               # token vector dimensionality
-    ndim2 = ndim>>1                     # for token encodings the first half is the word vector, the second half is the context vector
-    nfeat = size(f, 1)                  # number of features
-    smax = Pinf                         # maximum distance in sentence
-    i = 0                               # i: index into feature vector x
+function featurevec(p::Parser, s::Sentence, flist::FeatureList,
+                    x::AbstractArray=Array(wtype(s),flen(p,s,flist),1))
     fill!(x, zero(eltype(x)))
-
-    for j=1:nfeat               # j: index into feature matrix f
-
-	# identify the anchor a: 8.81us
-        if f[j,1] >= 0                       # buffer word
-            a = p.wptr + f[j,1]
-            (a > p.nword) && (a = 0)
-        else                                  # stack word
-            ax = p.sptr + f[j,1] + 1
-            if (ax > 0) 
-                a = p.stack[ax]
-                @assert ((a >= 1) && (a < p.wptr)) "Bad anchor"
-            else 
-                a = 0 
-	    end
-	end
-
-	# identify the target b: 3.13us
-	if a == 0
-	    b = 0
-	elseif f[j,2] == 0                   # self
-	    b = a                              
-	elseif f[j,2] > 0                    # right-child
-	    @assert (a < p.wptr) "buffer words do not have rdeps"
-	    nc = p.rcnt[a]
-	    bx = nc - f[j,2] + 1
-	    if (bx > 0)
-		b = p.rdep[a, bx]
-		@assert ((b > a) && (b <= p.nword) && (p.head[b] == a)) "Bad rdep"
-	    else
-		b = 0
-	    end
-	elseif f[j,2] < 0                    # left-child
-	    @assert (a <= p.wptr) "buffer words other than n0 do not have ldeps"
-	    nc = p.lcnt[a]
-	    bx = nc + f[j,2] + 1
-	    if (bx > 0)
-		b = p.ldep[a, bx]
-		@assert ((b >= 1) && (b < a) && (p.head[b] == a)) "Bad ldep"
-	    else
-		b = 0
-	    end
-	end
-
-	# generate the feature: 4.16us
-	if f[j,3] == 0	# wvec
-	    (b > 0) && (x[i+1:i+ndim] = s.wvec[:,b])
-	    i+=ndim; 
-
-	elseif f[j,3] == 1       # exists
-	    (b > 0) && (x[i+1] = 1)
-	    i+=1; 
-
-	elseif f[j,3] == -1      # does not exist
-	    (b == 0) && (x[i+1] = 1)
-	    i+=1; 
-
-	elseif f[j,3] == 2       # rdep count, 4 bits, == encoding
-	    if (b > 0) 
-		@assert (b < p.wptr) "buffer words do not have rdeps"
-		nc = p.rcnt[b]
-		(nc > 3) && (nc = 3)
-		x[i+1+nc] = 1
-	    end 
-	    i+=4; 
-
-	elseif f[j,3] == -2      # ldep count, 4 bits, == encoding
-	    if (b > 0)
-		@assert (b <= p.wptr) "buffer words other than n0 do not have ldeps"
-		nc = p.lcnt[b] 
-		(nc > 3) && (nc = 3)
-		x[i+1+nc] = 1
-	    end 
-	    i+=4; 
-
-	elseif f[j,3] == 3       # distance to the right
-	    @assert (f[j,1] < 0 && f[j,2] == 0) "distance only available for stack words"
-	    if (b > 0)
-		if f[j,1] == -1 # s0n0 distance
-		    c = (p.wptr <= p.nword ? p.wptr : b+smax) # root is far far away...
-		else      # s(i)-s(i-1) distance
-		    cx = p.sptr + f[j,1] + 2
-		    c = p.stack[cx]
-		end
-		@assert (c > b) "c <= b"
-		d = c - b
-		(d > 4) && (d = 4)
-		x[i+d] = 1
-	    end 
-	    i+=4; 
-
-	elseif f[j,3] == 4       # word (first) half of token vector
-	    (b > 0) && (for d=1:ndim2; x[i+d]=s.wvec[d,b]; end)
-	    i+=ndim2; 
-
-	elseif f[j,3] == -4      # context (second) half of token vector
-	    (b > 0) && (for d=1:ndim2; x[i+d]=s.wvec[ndim2+d,b]; end)
-	    i+=ndim2; 
-
-	elseif f[j,3] == 5       # rdep count, 4 bits, >= encoding
-	    if (b > 0) 
-		@assert (b < p.wptr) "buffer words do not have rdeps"
-		nc = p.rcnt[b]
-		for ic=1:4; (nc >= ic) && (x[i+ic] = 1); end
-	    end 
-	    i+=4; 
-
-	elseif f[j,3] == -5      # ldep count, 4 bits, >= encoding
-	    if (b > 0)
-		@assert (b <= p.wptr) "buffer words other than n0 do not have ldeps"
-		nc = p.lcnt[b]
-		for ic=1:4; (nc >= ic) && (x[i+ic] = 1); end
-	    end 
-	    i+=4; 
-
-	elseif f[j,3] == 6       # rdep count, 4 bits, <= encoding
-	    if (b > 0) 
-		@assert (b < p.wptr) "buffer words do not have rdeps"
-		nc = p.rcnt[b]
-		for ic=0:3; (nc <= ic) && (x[i+1+ic] = 1); end
-	    end 
-	    i+=4; 
-
-	elseif f[j,3] == -6      # ldep count, 4 bits, <= encoding
-	    if (b > 0)
-		@assert (b <= p.wptr) "buffer words other than n0 do not have ldeps"
-		nc = p.lcnt[b]
-		for ic=0:3; (nc <= ic) && (x[i+1+ic] = 1); end
-	    end 
-	    i+=4; 
-
-	elseif f[j,3] == 7       # distance to the right, >= encoding, 8 bits
-	    @assert (f[j,1] < 0 && f[j,2] == 0) "distance only available for stack words"
-	    if (b > 0)
-		if f[j,1] == -1 # s0n0 distance
-		    c = (p.wptr <= p.nword ? p.wptr : b+smax) # root is far far away...
-		else      # s(i)-s(i-1) distance
-		    cx = p.sptr + f[j,1] + 2
-		    c = p.stack[cx]
-		end
-		@assert (c > b) "c <= b"
-		d = c - b
-		dmin = [2,3,4,6,8,12,16,20]
-		for id=1:length(dmin); (d >= dmin[id]) && (x[i+id] = 1); end
-	    end 
-	    i+=8; 
-
-	elseif f[j,3] == -7       # distance to the right, <= encoding, 8 bits
-	    @assert (f[j,1] < 0 && f[j,2] == 0) "distance only available for stack words"
-	    if (b > 0)
-		if f[j,1] == -1 # s0n0 distance
-		    c = (p.wptr <= p.nword ? p.wptr : b+smax) # root is far far away...
-		else      # s(i)-s(i-1) distance
-		    cx = p.sptr + f[j,1] + 2
-		    c = p.stack[cx]
-		end
-		@assert (c > b) "c <= b"
-		d = c - b
-		dmax = [1,2,3,4,6,8,12,16]
-		for id=1:length(dmax); (d <= dmax[id]) && (x[i+id] = 1); end
-	    end 
-	    i+=8; 
-
-	elseif f[j,3] == -3      # avg of in-between token vectors to the right
-	    @assert (f[j,1] < 0 && f[j,2] == 0) "in-between only available for stack words"
-	    if (b > 0)
-		if f[j,1] == -1 # s0n0 interval
-		    c = (p.wptr <= p.nword ? p.wptr : b) # no in-between with root
-		else      # s(i)-s(i-1) interval
-		    cx = p.sptr + f[j,1] + 2
-		    c = p.stack[cx]
-		end
-		if c > b+1
-                    for d=1:ndim
-                        for bc=(b+1):(c-1)
-                            x[i+d] += s.wvec[d,bc]
-                        end
-                        x[i+d] /= (c-b-1)
-                    end
-                end
-	    end
-	    i+=ndim; 
-	    
-	elseif f[j,3] == 8      # avg of in-between word vectors to the right
-	    @assert (f[j,1] < 0 && f[j,2] == 0) "in-between only available for stack words"
-	    if (b > 0)
-		if f[j,1] == -1 # s0n0 interval
-		    c = (p.wptr <= p.nword ? p.wptr : b) # no in-between with root
-		else      # s(i)-s(i-1) interval
-		    cx = p.sptr + f[j,1] + 2
-		    c = p.stack[cx]
-		end
-		if c > b+1
-                    for d=1:ndim2
-                        for bc=(b+1):(c-1)
-                            x[i+d] += s.wvec[d,bc]
-                        end
-                        x[i+d] /= (c-b-1)
-                    end
-		end
-	    end
-	    i+=ndim2; 
-
-	elseif f[j,3] == -8      # avg of in-between context vectors to the right
-	    @assert (f[j,1] < 0 && f[j,2] == 0) "in-between only available for stack words"
-            if (b > 0)
-                if f[j,1] == -1 # s0n0 interval
-                    c = (p.wptr <= p.nword ? p.wptr : b) # no in-between with root
-                else      # s(i)-s(i-1) interval
-                    cx = p.sptr + f[j,1] + 2
-                    c = p.stack[cx]
-                end
-                if c > b+1
-                    for d=1:ndim2
-                        for bc=(b+1):(c-1)
-                            x[i+d] += s.wvec[ndim2+d,bc]
-                        end
-                        x[i+d] /= (c-b-1)
-                    end
+    nx = 0
+    nw = wdim(s) >> 1
+    x1 = one(eltype(x))
+    for f in flist
+        @assert in(f[1], "sn") "feature string should start with [sn]"
+        (i,n) = isdigit(f[2]) ? (f[2] - '0', 3) : (0, 2)
+        (a,d) = (0,0)           # target word index and right distance
+        if ((f[1] == 's') && (p.sptr - i >= 1))
+            a = p.stack[p.sptr - i]
+            d = (i>0) ? (p.stack[p.sptr - i + 1] - a) : (p.wptr - a)
+        elseif ((f[1] == 'n') && (p.wptr + i <= p.nword))
+            a = p.wptr + i
+        end
+        while (fn=f[n];in(fn, "hlr"))
+            d = 0
+            (i,n) = isdigit(f[n+1]) ? (f[n+1] - '0', n+2) : (1, n+1)
+            @assert i > 0 "hlr indexing is one based"
+            a == 0 && continue
+            if fn == 'l'
+                @assert (a <= p.wptr) "buffer words other than n0 do not have ldeps"
+                j = p.lcnt[a] - i + 1 # leftmost child at highest index
+                a = (j > 0) ? p.ldep[a,j] : 0
+            elseif fn == 'r'
+                @assert (a < p.wptr) "buffer words do not have rdeps"
+                j = p.rcnt[a] - i + 1
+                a = (j > 0) ? p.rdep[a,j] : 0
+            else # if fn == 'h'
+                for j=1:i
+                    a = p.head[a]
+                    a == 0 && break
                 end
             end
-            i+=ndim2; 
-
-	elseif f[j,3] == 9       # head exists
-	    (b > 0) && (x[i+1] = (p.head[b] > 0))
-	    i+=1; 
-
-	elseif f[j,3] == -9      # head does not exist
-	    (b > 0) && (x[i+1] = (p.head[b] == 0))
-	    i+=1; 
-
-	else
-	    error("Unknown feature $(f[j,3])")
-        end # if f[j,3]...
-    end # for j=1:nfeat
+        end
+        @assert n == length(f)
+        fn = f[n]
+        @assert in(fn, "wpdLabAB")
+        if fn == 'w'
+            (a > 0) && copy!(sub(x, nx+1:(nx+nw)), sub(s.wvec, 1:nw, int(a))); nx+=nw
+        elseif fn == 'p'
+            (a > 0) && copy!(sub(x, nx+1:(nx+nw)), sub(s.wvec, nw+1:nw+nw, int(a))); nx+=nw
+        elseif fn == 'd'
+            (a > 0) && (d > 0) && (x[nx+(d>10?6:d>5?5:d)] = x1); nx+=6
+        elseif fn == 'L'
+            (a > 0) && (x[nx+1+p.deprel[a]] = x1); nx+=(p.ndeps+1) # 0 is ROOT/NA
+        elseif fn == 'a'
+            (a > 0) && (x[nx+1+(p.lcnt[a]>9?9:p.lcnt[a])] = x1); nx+=10
+        elseif fn == 'b'
+            (a > 0) && (x[nx+1+(p.rcnt[a]>9?9:p.rcnt[a])] = x1); nx+=10
+        elseif fn == 'A'
+            (a > 0) && (for j=1:p.lcnt[a]; x[nx+p.deprel[p.ldep[a,j]]] = x1; end); nx+=p.ndeps
+        elseif fn == 'B'
+            (a > 0) && (for j=1:p.rcnt[a]; x[nx+p.deprel[p.rdep[a,j]]] = x1; end); nx+=p.ndeps
+        else
+            error("Unknown feature $(fn)")
+        end
+    end
+    @assert nx == length(x)
     return x
-end # features
+end
+
+function flen(p::Parser, s::Sentence, flist::FeatureList)
+    nx = 0
+    nw = wdim(s) >> 1
+    nd = p.ndeps
+    fs = "wpdLabAB"
+    dx = [nw,nw,6,nd+1,10,10,nd,nd]
+    for f in flist
+        i = search(fs, f[end])
+        @assert i>0
+        nx += dx[i]
+    end
+    return nx
+end
+
