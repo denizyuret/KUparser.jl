bf7882f32b9b122ab0300d204944f1c44e7f20f4
Deniz Yuret <denizyuret@gmail.com>
2015-03-27 23:20:22 +0200

    Added evalparse.

--- test/testhybrid.jl
+++ test/testhybrid.jl
@@ -4,8 +4,11 @@ using HDF5,JLD,KUparser
 @date s1 = d["corpus"][1]
 @date p1 = Parser{:ArcHybrid}(wcnt(s1), ndeps)
 @date r0 = rparse(:ArcHybrid, d["corpus"], ndeps)
+@show evalparse(r0, d["corpus"])
 @date f1 = Flist.tacl13hybrid
 @date r1 = oparse(:ArcHybrid, d["corpus"], f1, ndeps)
+@show evalparse(r1[1], d["corpus"])
 @date f2 = Flist.acl11eager
 @date r2 = oparse(:ArcHybrid, d["corpus"], f2, ndeps)
+@show evalparse(r2[1], d["corpus"])
 :ok
