26dcdaac8b53f7f89ea846a5c5d0a105b34c6694
Deniz Yuret <denizyuret@gmail.com>
2015-03-31 00:13:19 +0300

    Added size test.

--- test/testarctypes.jl
+++ test/testarctypes.jl
@@ -1,21 +1,18 @@
-using HDF5,JLD,KUparser
+using HDF5,JLD,KUparser,Base.Test
 @date d = load("conll07.tst.jld4")
 @show ndeps = length(d["deprel"])
 @date corpus = d["corpus"]
-@date s1 = corpus[1]
+@date feats = Flist.acl11eager
+
+# We try each arctype with rparse and oparse
+# They will complain if movecosts or nmoves is buggy
 for pt in (ArcEager13, ArcEagerR1, ArcHybrid13, ArcHybridR1)
     @show pt
-    @date r0 = rparse(pt, corpus, ndeps)
-    @show evalparse(r0, corpus)
-    @date r1 = oparse(pt, corpus, ndeps)
-    @show evalparse(r1, corpus)
-    @date r2 = oparse(pt, corpus, ndeps)
-    @show evalparse(r2, corpus)
-    # @date p1 = pt(wcnt(s1), ndeps)
-    # @date f1 = Flist.acl11eager
-    # @date net = KUnet.newnet(KUnet.relu, KUparser.flen(p1,s1,f1), 20000, p1.nmove)
-    # @date r3 = gparse(pt, corpus, net, f1, ndeps)
-    # @show evalparse(r3, corpus)
-    # @date r4 = gparse(pt, corpus, net, f1, ndeps, 9999)
-    # @show evalparse(r4, corpus)
+    @date r = rparse(pt, corpus, ndeps)
+    @show evalparse(r, corpus)
+    @date (p,x,y) = oparse(pt, corpus, ndeps, feats)
+    @show evalparse(p, corpus)
+    p1 = pt(1, ndeps)
+    @test @show size(x) == KUparser.xsize(p1, corpus, feats)
+    @test @show size(y) == KUparser.ysize(p1, corpus)
 end
