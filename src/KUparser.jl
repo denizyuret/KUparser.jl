e9d7d80e02e8536cc8c2a07293f41164861f5e49
Deniz Yuret <denizyuret@gmail.com>
2015-03-31 01:01:38 +0300

    export pxyequal.

--- src/KUparser.jl
+++ src/KUparser.jl
@@ -10,10 +10,10 @@ typealias ParserType Symbol
 # include("arceager13.jl");
 # include("archybrid13.jl");
 
-include("util.jl");	  export @date, evalparse
+include("util.jl");	  export @date, evalparse, pxyequal
 include("sentence.jl");   export Sentence, Corpus, wdim, wcnt, wtype
 include("parser.jl");     export ArcEager13, ArcEagerR1, ArcHybrid13, ArcHybridR1
-include("features.jl");   export Feature, Fvec
+include("features.jl");   export Feature, Fvec, flen, xsize, ysize
 include("gparser.jl");    export gparse
 include("bparser.jl");    export bparse
 include("oparser.jl");    export oparse
