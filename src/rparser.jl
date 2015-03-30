527d97861a5494aa380c4c82c457ecf3af28ac03
Deniz Yuret <denizyuret@gmail.com>
2015-03-30 20:05:30 +0300

    New interface.

--- src/rparser.jl
+++ src/rparser.jl
@@ -1,8 +1,8 @@
 # The random parser executes random valid moves to test the cost fn.
 
-function rparse(pt::ParserType, s::Sentence, ndeps::Integer)
-    (p, c) = initrparse(pt, s, ndeps)
-    cost = 0
+function rparse(p::Parser, s::Sentence, ndeps::Integer)
+    c = Array(Position, p.nmove)
+    totalcost = 0
     while anyvalidmoves(p)
         movecosts(p, s.head, s.deprel, c)
         if rand() < 0.1
@@ -11,38 +11,44 @@ function rparse(pt::ParserType, s::Sentence, ndeps::Integer)
         else
             r = indmin(c)
         end
-        cost += c[r]
+        totalcost += c[r]
         move!(p, r)
     end
-    @assert cost == sum((p.head .!= s.head) | (p.deprel .!= s.deprel))
+    @assert totalcost == truecost(p, s)
     return p
 end
 
-function rparse(pt::ParserType, c::Corpus, ndeps::Integer)
-    map(s->rparse(pt,s,ndeps), c)
+function rparse{T<:Parser}(pt::Type{T}, s::Sentence, ndeps::Integer)
+    p = pt(wcnt(s), ndeps)
+    rparse(p, s, ndeps)
 end
 
-function rparse(pt::ParserType, c::Corpus, ndeps::Integer, ncpu::Integer)
-    Main.resetworkers(ncpu)
-    d = distproc(c, workers()[1:ncpu])
-    @everywhere gc()
-    p = pmap(procs(d)) do x
-        rparse(pt, localpart(d), ndeps)
+function rparse{T<:Parser}(pt::Type{T}, c::Corpus, ndeps::Integer)
+    pa = map(s->pt(wcnt(s), ndeps), c)
+    rparse(pa, c, ndeps)
+end
+
+function rparse{T<:Parser}(pa::Vector{T}, c::Corpus, ndeps::Integer)
+    for i=1:length(c); rparse(pa[i], c[i], ndeps); end
+    return pa
+end
+
+function rparse{T<:Parser}(pt::Type{T}, c::Corpus, ndeps::Integer, ncpu::Integer)
+    @date Main.resetworkers(ncpu)
+    sa = distribute(c)                                  # distributed sentence array
+    pa = map(s->pt(wcnt(s), ndeps), sa)                 # distributed parser array
+    @sync for p in procs(sa)
+        @spawnat p oparse(localpart(pa), localpart(sa), f, ndeps)
     end
-    Main.rmworkers()
-    return p
+    pa = convert(Vector{pt}, pa)
+    @date Main.rmworkers()
+    return pa
 end
 
-function initrparse(pt::ParserType, s::Sentence, ndeps::Integer)
-    (ndims, nword) = size(s.wvec)
-    p = Parser{pt}(nword, ndeps)
-    c = Array(Position, p.nmove)
-    (p, c)
-end
-
-function rparse_dbg(pt::ParserType, s::Sentence, ndeps::Integer)
+function rparse_dbg{T<:Parser}(pt::Type{T}, s::Sentence, ndeps::Integer)
     dump(vcat([1:wcnt(s)]', s.head', s.deprel'))
-    (p, c) = initrparse(pt, s, ndeps)
+    p = pt(wcnt(s), ndeps)
+    c = Array(Position, p.nmove)
     cost = 0
     while anyvalidmoves(p)
         movecosts(p, s.head, s.deprel, c)
@@ -56,6 +62,6 @@ function rparse_dbg(pt::ParserType, s::Sentence, ndeps::Integer)
         move!(p, r)
         println("$((r,int(c[r]),cost)) $(int(p.stack[1:p.sptr])) $(p.wptr) $(int(c))")
     end
-    @assert cost == sum((p.head .!= s.head) | (p.deprel .!= s.deprel))
+    @assert cost == truecost(p,s)
     return p
 end
