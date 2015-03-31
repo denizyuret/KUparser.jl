4361f78e4f2ae7efc5a096e8b94ddef7656f9a26
Deniz Yuret <denizyuret@gmail.com>
2015-03-31 17:48:01 +0300

    *** empty log message ***

---
+++ test/bpmemtest.jl
@@ -0,0 +1,19 @@
+using HDF5,JLD,KUparser,KUnet,Base.Test
+@date @load "acl11.trn.jld4"
+@show net=newnet("bp10acl11eager13.jld")
+ft = Flist.acl11eager
+pt = ArcEager13
+ncpu = 20
+ndeps = length(deprel)
+nbatch = 128
+nbeam = 64
+@date pxy1=bparse(pt, corpus, ndeps, ft, net, nbeam, nbatch, ncpu; xy=true)
+@date pxy2=bparse(pt, corpus, ndeps, ft, net, nbeam, nbatch; xy=true)
+@test @show isequal(pxy1, pxy2)
+
+# Space: Need ncpu x pbatch x nbeam < 7680 to fit in K20 memory
+
+# * iui-k40-memory: Each K40 has 11.5GB for a total of 23GB.  The
+# ZN11 ArcEager13 setup takes in MB GPU memory:
+#   280*ncpu+0.1*ncpu*nbeam*nbatch
+# For ncpu=20, nbeam=64, nbatch=128 we hit the upper limit.
