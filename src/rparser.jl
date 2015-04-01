e3137564d414d97f57eed9e42d4460572134504a
Deniz Yuret <denizyuret@gmail.com>
2015-04-01 19:56:22 +0300

    Call 1-cpu version if ncpu=1.

--- src/rparser.jl
+++ src/rparser.jl
@@ -1,4 +1,34 @@
 # The random parser executes random valid moves to test the cost fn.
+#
+# The public interface for rparse takes the following arguments:
+#
+# pt::ParserType: ArcHybridR1, ArcEagerR1, ArcHybrid13, ArcEager13
+# s::Sentence or c::Corpus: input sentence(s), a single parser is returned for s, a vector of parsers for c
+# ndeps::Integer: number of dependency types
+# ncpu::Integer: (optional) performs parallel processing
+
+function rparse{T<:Parser}(pt::Type{T}, s::Sentence, ndeps::Integer)
+    p = pt(wcnt(s), ndeps)
+    rparse(p, s, ndeps)
+end
+
+function rparse{T<:Parser}(pt::Type{T}, c::Corpus, ndeps::Integer)
+    pa = map(s->pt(wcnt(s), ndeps), c)
+    rparse(pa, c, ndeps)
+end
+
+function rparse{T<:Parser}(pt::Type{T}, c::Corpus, ndeps::Integer, ncpu::Integer)
+    (ncpu == 1) && return rparse(pt, c, ndeps)
+    @date Main.resetworkers(ncpu)
+    sa = distribute(c)                                  # distributed sentence array
+    pa = map(s->pt(wcnt(s), ndeps), sa)                 # distributed parser array
+    @sync for p in procs(sa)
+        @spawnat p oparse(localpart(pa), localpart(sa), f, ndeps)
+    end
+    pa = convert(Vector{pt}, pa)
+    @date Main.rmworkers()
+    return pa
+end
 
 function rparse(p::Parser, s::Sentence, ndeps::Integer)
     c = Array(Position, p.nmove)
@@ -18,30 +48,8 @@ function rparse(p::Parser, s::Sentence, ndeps::Integer)
     return p
 end
 
-function rparse{T<:Parser}(pt::Type{T}, s::Sentence, ndeps::Integer)
-    p = pt(wcnt(s), ndeps)
-    rparse(p, s, ndeps)
-end
-
-function rparse{T<:Parser}(pt::Type{T}, c::Corpus, ndeps::Integer)
-    pa = map(s->pt(wcnt(s), ndeps), c)
-    rparse(pa, c, ndeps)
-end
-
 function rparse{T<:Parser}(pa::Vector{T}, c::Corpus, ndeps::Integer)
     for i=1:length(c); rparse(pa[i], c[i], ndeps); end
-    return pa
-end
-
-function rparse{T<:Parser}(pt::Type{T}, c::Corpus, ndeps::Integer, ncpu::Integer)
-    @date Main.resetworkers(ncpu)
-    sa = distribute(c)                                  # distributed sentence array
-    pa = map(s->pt(wcnt(s), ndeps), sa)                 # distributed parser array
-    @sync for p in procs(sa)
-        @spawnat p oparse(localpart(pa), localpart(sa), f, ndeps)
-    end
-    pa = convert(Vector{pt}, pa)
-    @date Main.rmworkers()
     return pa
 end
 
