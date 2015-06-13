403fbbf060e0d77ac8cbc12c11daca22bf9ce7a6
Deniz Yuret <denizyuret@gmail.com>
2015-06-13 03:44:19 +0300

    Added sfeatures.jl.

--- src/KUparser.jl
+++ src/KUparser.jl
@@ -6,16 +6,13 @@ using KUnet
 include("util.jl");	  export @date, evalparse, pxyequal, testnet
 include("sentence.jl");   export Sentence, Corpus, wdim, wcnt, wtype
 include("parser.jl");     export ArcEager13, ArcEagerR1, ArcHybrid13, ArcHybridR1, Parser, reset!
-include("features.jl");   export Feature, Fvec, flen, xsize, ysize
+include("sfeatures.jl");
+include("features.jl");   export Fvec, flen, xsize, ysize
 include("gparser.jl");    export gparse
 include("bparser.jl");    export bparse
 include("oparser.jl");    export oparse
 include("rparser.jl");    export rparse
 include("flist.jl");      export Flist
-
-# Setting for dense vs sparse features:
-features = dfeatures
-# features = sfeatures
 
 end # module
 
