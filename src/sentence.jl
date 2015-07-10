6de1b5470811f672dcb6dbccf2c00858081b9768
Deniz Yuret <denizyuret@gmail.com>
2015-07-10 00:01:04 -0700

    *** empty log message ***

--- src/sentence.jl
+++ src/sentence.jl
@@ -6,3 +6,9 @@ wtype(s::Sentence)=eltype(s.wvec)
 wdim(c::Corpus)=size(c[1].wvec,1)
 wcnt(c::Corpus)=(n=0;for s in c; n+=wcnt(s); end; n)
 wtype(c::Corpus)=eltype(c[1].wvec)
+
+function Base.show(io::IO, s::Sentence)
+    print(io, (wcnt(s) <= 6 ?
+               join(s.form, " ") :
+               join([s.form[1:3], "...", s.form[end-2:end]], " ")))
+end
