13cee380c2d438bdbcad33f4b52c0e9a00c2578a
Deniz Yuret <denizyuret@gmail.com>
2015-07-28 12:10:54 -0700

    Split up bparse.

--- src/bparser.jl
+++ src/bparser.jl
@@ -26,65 +26,33 @@ type Beam
     end
 end
 
-function bparse{T<:Parser}(pt::Type{T}, corpus::Corpus, ndeps::Integer,
-                           feats::Fvec, net::Net, nbeam::Integer, 
-                           nbatch::Integer=1; xy::Bool=false)
+typealias Batch Vector{Beam}
+
+
+
+function bparse{T<:Parser}(pt::Type{T}, corpus::Corpus, ndeps::Int,
+                           feats::DFvec, net::Net, nbeam::Int, 
+                           nbatch::Int=1; xy::Bool=false)
     xy && error("xy not implemented yet.")
     (nbatch < 1 || nbatch > length(corpus)) && (nbatch = length(corpus))
-    parses = pt[]
+    (parses, batch, fmatrix, score) = bp_init(pt, corpus, ndeps, feats)
     for s1=1:nbatch:length(corpus)
         s2=min(length(corpus), s1+nbatch-1)                                     
-        (batch, fmatrix, score, cost) = initbatch(pt, corpus, s1:s2, ndeps, feats, nbeam)
+        bp_resize!(pt, corpus, s1:s2, ndeps, nbeam, batch, fmatrix, score) # 34
         nf = 0
         while true
-            ### Compute features
             nf1 = nf + 1
-            for b in batch
-                xy && (b.mincost != 0) && continue
-                anyvalidmoves(b.beam[1].parser) || continue
-                for bs in b.beam
-                    f = features(bs.parser, b.sent, feats, fmatrix, (nf+=1)) # 8258
-                    bs.fidx = nf
-                end
-            end
-            nf2 = nf
+            nf2 = bp_features(batch, feats, fmatrix.arr, nf, xy) # 6431
             nf2 < nf1 && break
-
-            ### Compute scores
-            predict(net, sub(fmatrix,:,nf1:nf2), sub(score,:,nf1:nf2); batch=0) # 12783
-
-            ### Update beam
+            predict(net, sub(fmatrix,:,nf1:nf2), sub(score,:,nf1:nf2); batch=0) # 12785
             for b in batch
                 xy && (b.mincost != 0) && continue
                 anyvalidmoves(b.beam[1].parser) || continue
-
-                ### Compute candidates
-                resize!(b.cand,0)
-                for bs in b.beam
-                    movecosts(bs.parser, b.sent.head, b.sent.deprel, cost) # 670
-                    for j=1:length(cost)
-                        cost[j] == typemax(Cost) && continue
-                        push!(b.cand, BeamState(bs, j, bs.score + score[j,bs.fidx], bs.cost + cost[j])) # 1496
-                        # parser and fidx are uninitialized in the candidate
-                    end
-                end
-                @assert (length(b.cand) > 0) "No candidates found"
-
-                ### Sort candidates
-                sort!(b.cand; rev=true) # 338
-
-                ### Top candidates to the beam, update mincost
-                resize!(b.beam,0)
-                b.mincost = typemax(Cost)
-                for bc in b.cand
-                    length(b.beam) == nbeam && break
-                    bc.cost < b.mincost && (b.mincost = bc.cost)
-                    bc.parser = copy(bc.parent.parser)     # 2201
-                    move!(bc.parser, bc.move)              # 265
-                    push!(b.beam, bc)                      # 10
-                end
-            end # for b in batch
-        end # while true
+                bp_update_cand(b, score.arr) # 568
+                sort!(b.cand; rev=true) # 339
+                bp_update_beam(b, nbeam) # 369
+            end
+        end
         for b in batch
             push!(parses, b.beam[1].parser)
             # TODO: go back on beams and push x/y using fmatrix and BeamState.move,score
@@ -93,21 +61,66 @@ function bparse{T<:Parser}(pt::Type{T}, corpus::Corpus, ndeps::Integer,
     return parses
 end # function bparse
 
-function initbatch{T<:Parser}(pt::Type{T}, corpus::Corpus, r::UnitRange{Int}, ndeps::Integer, feats::DFvec, nbeam::Integer)
-    batch = [Beam(pt, corpus[i], ndeps) for i in r]
-    s1 = batch[1].sent
-    p1 = batch[1].beam[1].parser
-    nmove = p1.nmove
-    ftype = wtype(s1)
-    frows = flen(p1, s1, feats)
-    nword = 0; for i in r; nword += wcnt(corpus[i]); end
-    fcols = 2 * nword * nbeam
-    fmatrix=Array(ftype, frows, fcols)
-    score=Array(ftype, nmove, fcols)
-    cost=Array(Cost, nmove)
-    return (batch, fmatrix, score, cost)
+function bp_init{T<:Parser}(pt::Type{T}, corpus::Corpus, ndeps::Int, feats::DFvec)
+    parses = pt[]
+    batch = Array(Beam, 0)
+    s0 = corpus[1]
+    p0 = pt(wcnt(s0),ndeps)
+    fmatrix = KUdense(Array, wtype(corpus), (flen(p0,s0,feats), 0))
+    score = KUdense(Array, wtype(corpus), (p0.nmove, 0))
+    return (parses, batch, fmatrix, score)
 end
 
+function bp_resize!{T<:Parser}(pt::Type{T}, corpus::Corpus, r::UnitRange{Int}, ndeps::Int, nbeam::Int, 
+                               batch::Batch, fmatrix::KUdense, score::KUdense)
+    batch = resize!(batch, length(r))
+    nword = 0
+    j = 0
+    for i in r
+        batch[j+=1] = Beam(pt, corpus[i], ndeps)
+        nword += wcnt(corpus[i])
+    end
+    fcols = 2*nword*nbeam
+    resize!(fmatrix, (size(fmatrix,1), fcols))
+    resize!(score, (size(score,1), fcols))
+end
+
+function bp_features(batch::Batch, feats::DFvec, fmatrix::Matrix, nf::Int, xy::Bool)
+    for b in batch
+        xy && (b.mincost != 0) && continue
+        anyvalidmoves(b.beam[1].parser) || continue
+        for bs in b.beam
+            f = features(bs.parser, b.sent, feats, fmatrix, (nf+=1)) # 6589
+            bs.fidx = nf
+        end
+    end
+    return nf
+end
+
+function bp_update_cand(b::Beam, score::Matrix)
+    resize!(b.cand,0)
+    cost = Array(Cost, size(score,1))
+    for bs in b.beam
+        movecosts(bs.parser, b.sent.head, b.sent.deprel, cost) # 162
+        for j=1:length(cost)
+            cost[j] == typemax(Cost) && continue
+            push!(b.cand, BeamState(bs, j, bs.score + score[j,bs.fidx], bs.cost + cost[j])) # 1242
+        end
+    end
+    (length(b.cand) > 0) || error("No candidates found")
+end
+
+function bp_update_beam(b::Beam, nbeam::Int)
+    resize!(b.beam,0)
+    b.mincost = typemax(Cost)
+    for bc in b.cand
+        length(b.beam) == nbeam && break
+        bc.cost < b.mincost && (b.mincost = bc.cost)
+        bc.parser = copy(bc.parent.parser)     # 378
+        move!(bc.parser, bc.move)              # 20
+        push!(b.beam, bc)                      # 3
+    end
+end
 
 # Data structures for beam search:
 
@@ -131,3 +144,22 @@ end
 
 # beam: nbeam, beam, sent, mcs, cand, beam2; do we need?
 # prealloc parsers but no prealloc of beamstates? parsers dynamic too.
+
+
+### DEAD CODE
+
+
+#     batch = [Beam(pt, corpus[i], ndeps) for i in r]
+#     s1 = batch[1].sent
+#     p1 = batch[1].beam[1].parser
+#     nmove = p1.nmove
+#     ftype = wtype(s1)
+#     frows = flen(p1, s1, feats)
+#     nword = 0; for i in r; nword += wcnt(corpus[i]); end
+#     fcols = 2 * nword * nbeam
+#     fmatrix=Array(ftype, frows, fcols)
+#     score=Array(ftype, nmove, fcols)
+#     cost=Array(Cost, nmove)
+#     return (batch, fmatrix, score, cost)
+# end
+
