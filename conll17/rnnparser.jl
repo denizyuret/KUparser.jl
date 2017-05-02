3c9405d50e6ee00c8eaab42b6863813c8e559831
Deniz Yuret <denizyuret@gmail.com>
2017-05-02 14:00:16 +0300

    fixed convert2cpu.

--- conll17/rnnparser.jl
+++ conll17/rnnparser.jl
@@ -879,6 +879,8 @@ end
 map2cpu(x)=(if isbits(x); x; else; map2cpu2(x); end)
 map2cpu(x::KnetArray)=Array(x)
 map2cpu(x::Tuple)=map(map2cpu,x)
+map2cpu(x::AbstractString)=x
+map2cpu(x::DataType)=x
 map2cpu(x::Array)=map(map2cpu,x)
 map2cpu{T<:Number}(x::Array{T})=x
 map2cpu(x::Associative)=(y=Dict();for (k,v) in x; y[k] = map2cpu(x[k]); end; y)
@@ -886,6 +888,8 @@ map2cpu2(x)=(y=deepcopy(x); for f in fieldnames(x); setfield!(y,f,map2cpu(getfie
 
 map2gpu(x)=(if isbits(x); x; else; map2gpu2(x); end)
 map2gpu(x::KnetArray)=x
+map2gpu(x::AbstractString)=x
+map2gpu(x::DataType)=x
 map2gpu(x::Tuple)=map(map2gpu,x)
 map2gpu(x::Array)=map(map2gpu,x)
 map2gpu{T<:AbstractFloat}(x::Array{T})=KnetArray(x)
